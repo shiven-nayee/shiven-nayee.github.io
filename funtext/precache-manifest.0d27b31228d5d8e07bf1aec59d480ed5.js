fb664a59314dc0453d3614fb5c96482760f1f485
Shiven Nayee <shiven.nayee@gmail.com>
2019-12-05 19:35:31 -0500

    Added funtext site

---
+++ funtext/precache-manifest.0d27b31228d5d8e07bf1aec59d480ed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7b82129fd242a96f85b0e0cf961baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de2ef9e3898e5b8a1381",
+    "url": "/static/css/main.e1b2117a.chunk.css"
+  },
+  {
+    "revision": "f60aceff325870399ca7",
+    "url": "/static/js/2.3e5dfbd1.chunk.js"
+  },
+  {
+    "revision": "de2ef9e3898e5b8a1381",
+    "url": "/static/js/main.01329cc0.chunk.js"
+  },
+  {
+    "revision": "8bab9afcf99b56f5d1c2",
+    "url": "/static/js/runtime-main.dad7dafd.js"
+  }
+]);
