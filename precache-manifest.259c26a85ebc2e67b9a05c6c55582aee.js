efb0a06386305b11e076f3e7aecdb18377fbb69f
Shiven Nayee <shiven.nayee@gmail.com>
2020-01-31 15:27:22 -0500

    Updated to include new webpage, and new projects

---
+++ precache-manifest.259c26a85ebc2e67b9a05c6c55582aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384f0695bb5acae78e11be4d44db8d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2efebd6944c4a6f3dc14",
+    "url": "/static/css/main.43210c23.chunk.css"
+  },
+  {
+    "revision": "80983aa95d291afb9e0d",
+    "url": "/static/js/2.38f9637d.chunk.js"
+  },
+  {
+    "revision": "2efebd6944c4a6f3dc14",
+    "url": "/static/js/main.c8d9a95f.chunk.js"
+  },
+  {
+    "revision": "1a59c60cc783b6f8ee61",
+    "url": "/static/js/runtime-main.0695f50b.js"
+  }
+]);
